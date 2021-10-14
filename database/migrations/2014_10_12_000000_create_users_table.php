a702ea18a05ac97312a3c3e07b1534437fdd5c81
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-10-14 20:56:52 +0100

    feat: update upadte-profile

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('image_url')->nullable();
+            $table->string('bio')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
