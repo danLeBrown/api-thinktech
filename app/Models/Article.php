2fb63ae288835fc530eccb73ba58eede89b205da
Ayomide-Daniel <ayomidedaniel00@gmail.com>
2021-10-15 21:13:01 +0100

    feat: profile update

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -10,6 +10,8 @@ class Article extends Model
 {
     use HasFactory, TimeagoTrait;
 
+    protected $primaryKey = 'id';
+    
     protected $fillable = [
         'user_id',
         'title',
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    protected $primaryKey = 'id';
 
     public function role()
     {
